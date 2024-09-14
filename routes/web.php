73e24378d19dab1ad38a9aeb8554e6598efa39ef
Faqih <faqih0806@gmail.com>
2024-09-14 23:27:08 +0700

    Jobsheet3_DB_FACADE

--- routes/web.php
+++ routes/web.php
@@ -1,27 +1,22 @@
 <?php
 
+use App\Http\Controllers\KategoriController;
+use App\Http\Controllers\LevelController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\HomeController;
-use App\Http\Controllers\ProductController;
-use App\Http\Controllers\UserController;
-use App\Http\Controllers\SalesController;
 
-// Route untuk halaman Home
-Route::get('/', [HomeController::class, 'index']);
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
 
-// Route untuk halaman Products
-Route::get('/products', [ProductController::class, 'index']);
-
-// Route Group dengan Prefix untuk Product Category
-Route::prefix('category')->group(function () {
-    Route::get('/food-beverage', [ProductController::class, 'category'])->name('category.food-beverage')->defaults('category', 'food-beverage');
-    Route::get('/beauty-health', [ProductController::class, 'category'])->name('category.beauty-health')->defaults('category', 'beauty-health');
-    Route::get('/home-care', [ProductController::class, 'category'])->name('category.home-care')->defaults('category', 'home-care');
-    Route::get('/baby-kid', [ProductController::class, 'category'])->name('category.baby-kid')->defaults('category', 'baby-kid');
+Route::get('/', function() {
+    return view('welcome');
 });
 
-// Route untuk halaman User
-Route::get('/user/{id}/name/{name}', [UserController::class, 'profile']);
-
-// Route untuk halaman Penjualan
-Route::get('/sales', [SalesController::class, 'index']);
+Route::get('/level', [LevelController::class, 'index']);
