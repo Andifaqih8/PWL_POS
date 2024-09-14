96143fae275eac3ee654e279f25ed63f3c904a6d
Faqih <faqih0806@gmail.com>
2024-09-14 23:52:34 +0700

    Jobsheet3_ELOQUENT ORM

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,12 +2,15 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Models\UserModel;
 
 class UserController extends Controller
 {
     public function profile($id, $name)
     {
-        return view('/user', ['id' => $id, 'name' => $name]); 
+        $user = UserModel::all();
+
+        return view ('user', ['data' => $user]);
+
     }
 }
