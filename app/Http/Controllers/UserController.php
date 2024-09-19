d7f72d4429efbab42b35dbc638284317c388824c
Faqih <faqih0806@gmail.com>
2024-09-19 23:11:38 +0700

    JS4_Praktikum 2.1

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,8 +9,8 @@ use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
-    public function index()
-    {
+    // public function index()
+    // {
         //Praktikum 6.8
       //      $data = [
       //          'username' => 'customer-1',
@@ -37,17 +37,30 @@ class UserController extends Controller
             //           UserModel::create($data);
 
             //Prak 1.5
-            $data = [
-                        'level_id' => 2,  
-                        'username' => 'manager_tiga',
-                        'nama' => 'Manager 3',
-                        'password' => Hash::make('12345'),
+            // $data = [
+            //             'level_id' => 2,  
+            //             'username' => 'manager_tiga',
+            //             'nama' => 'Manager 3',
+            //             'password' => Hash::make('12345'),
                           
-                      ];
-                      UserModel::create($data);
+            //           ];
+            //           UserModel::create($data);
 
-        $user = UserModel::all();
-        return view ('user', ['data' => $user]);
+    //     $user = UserModel::find(1);
+    //     return view ('user', ['data' => $user]);
 
-    }   
+    //} 
+     public function index()
+     {
+
+       // $user = UserModel::find(1);
+       // $user = UserModel::where('level_id', 1)->first();
+       // $user = UserModel::firstWhere('level_id', 1);
+       $user = UserModel::findor(20, ['username', 'nama'], function() {
+        abort(404);
+     });
+        return view('user',['data' => $user]);
+    }
+
+
 }
