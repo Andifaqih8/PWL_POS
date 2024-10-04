268bf97f6aec360ed99aa4e65a20a88f36f87e18
Faqih <faqih0806@gmail.com>
2024-10-04 22:46:46 +0700

    JS 6

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -1,11 +1,12 @@
 @extends('layouts.template')
+
 @section('content')
 <div class="card card-outline card-primary">
     <div class="card-header">
         <h3 class="card-title">{{ $page->title }}</h3>
         <div class="card-tools">
-        <a class="btn btn-sm btn-primary mt-1" href="{{ url('user/create') }}">Tambah</a>
-        <button onclick="modalAction('{{ url('user/create_ajax') }}')" class="btn btn-sm btn-success mt-1">Tambah Ajax</button>
+            <a class="btn btn-sm btn-primary mt-1" href="{{ url('user/create') }}">Tambah</a> 
+            <button onclick="modalAction('{{ url('user/create_ajax') }}')" class="btn btn-sm btn-success mt-1">Tambah Ajax</button>
         </div>
     </div>
     <div class="card-body">
@@ -18,25 +19,24 @@
             <div class="alert alert-danger">
                 {{ session('error') }}
             </div>
-            @endif
-            <div class="row">
-                <div class="col-md-12">
-                    <div class="form-group row">
-                        <label class="col-1 control-label col-form-label">Filter:</label>
-                        <div class="col-3">
-                            <select class="form-control" id="level_id" name="level_id" required>
-                                <option value="">- Semua -</option>
-                                @foreach ($level as $item)
-                                    <option value="{{ $item->level_id }}">{{ $item->level_nama }}</option>
-                                @endforeach
-                            </select>
-                        </div>
-                        <small class="form-text text-muted">Level Pengguna</small>
+        @endif
+        <div class="row">
+            <div class="col-md-12">
+                <div class="form-group row">
+                    <label class="col-1 control-label col-form-label">Filter:</label>
+                    <div class="col-3">
+                        <select class="form-control" id="level_id" name="level_id" required>
+                            <option value="">- Semua -</option>
+                            @foreach ($level as $item)
+                                <option value="{{ $item->level_id }}">{{ $item->level_nama }}</option>
+                            @endforeach
+                        </select>
                     </div>
+                    <small class="form-text text-muted">Level Pengguna</small>
                 </div>
             </div>
         </div>
-            <table class="table table-bordered table-striped table-hover table-sm" id="table_user">
+        <table class="table table-bordered table-striped table-hover table-sm" id="table_user">
             <thead>
                 <tr>
                     <th>ID</th>
@@ -49,21 +49,23 @@
         </table>
     </div>
 </div>
-<div id="myModal" class="modal fade animate shake" tabindex="-1" role="dialog" data
-backdrop="static" data-keyboard="false" data-width="75%" aria-hidden="true">
-</div>
+<div id="myModal" class="modal fade animate shake" tabindex="-1" role="dialog" databackdrop="static" data-keyboard="false" data-width="75%" aria-hidden="true"></div>
 @endsection
+
 @push('css')
 @endpush
+
 @push('js')
     <script>
-                function modalAction(url = ''){ 
-                $('#myModal').load(url,function(){ 
-                    $('#myModal').modal('show'); 
-                }); 
-            }
+        function modalAction(url = '') {
+            $('#myModal').load(url, function() {
+                $('#myModal').modal('show');
+            });
+        }
+
+        var dataUser;
+        $(document).ready(function() {
             var dataUser = $('#table_user').DataTable({
-                $(document).ready(function() {
                 // serverSide: true, jika ingin menggunakan server side processing
                 serverSide: true,
                 ajax: {
@@ -105,7 +107,8 @@ backdrop="static" data-keyboard="false" data-width="75%" aria-hidden="true">
                     searchable: false
                 }]
             });
-            $('#level_id').on('change', function(){
+
+            $('#level_id').on('change', function() {
                 dataUser.ajax.reload();
             });
         });
